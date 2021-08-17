fb041522cfb7a52ce630a11354bc08172f93c05c
MrSkyGold <devmarcos.py@gmail.com>
2021-08-17 13:34:11 -0300

    adicionado validaçoes no campo contato

--- gabrielearq/core/urls.py
+++ gabrielearq/core/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('',views.index, name='index'),
     path('contato',views.contato, name='contato'),
     path('galeria', views.galeria, name='galeria'),
+    path('sobre', views.sobre, name='sobre'),
     path('galeria/<int:pk>', views.foto, name='foto')
 ]
--- gabrielearq/core/views.py
+++ gabrielearq/core/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from core.models import Contato
+from django.shortcuts import redirect, render
 from core.models import Galeria, Categoria
+from core.forms import FormContato
+from django.core.validators import validate_email
+from django.contrib import messages
 
 # Create your views here.
 
@@ -18,9 +22,28 @@ def galeria(request):
     context = {'categorias':categorias, 'fotos':fotos}
     return render(request, 'core/galeria.html', context)
 
+def sobre(request):
+    return render(request, 'core/sobre.html')
+
 def foto(request,pk):
     foto = Galeria.objects.get(id=pk)
     return render(request,'core/foto.html', {'foto':foto})
 
 def contato(request):
-    return render(request, 'core/contato.html')
+    if request.method != 'POST':
+        return render(request, 'core/contato.html')
+    nome = request.POST.get('nome')
+    email = request.POST.get('email')
+    assunto = request.POST.get('assunto')
+    mensagem = request.POST.get('mensagem')
+    if not nome or not email or not assunto or not mensagem:
+        messages.error(request, 'Nenhum campo pode estar em branco')
+        return render(request, 'core/contato.html')
+    try:
+        validate_email(email)
+    except:
+        return render(request, 'core/contato.html')
+    messages.success(request,'Sua mensagem foi enviada, em breve retornaremos o contato.')
+    contato = Contato.objects.create(nome=nome, email=email, assunto=assunto, mensagem=mensagem)
+    contato.save()
+    return redirect('contato')
